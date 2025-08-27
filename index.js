9314fa181eda04f6cce865264edddf5b6e04d813
saikatse85 <saikatse@gmail.com>
2025-08-28 00:50:55 +0600

    Added js click function

--- index.js
+++ index.js
@@ -7,3 +7,44 @@
             loveCount.innerText = count;
         });
     }
+
+//call Button clicked
+const totalCoin =document.getElementById("available-coin")
+const nationalTitle = document.getElementById("national-title")
+const nationalNumber = document.getElementById("national-number")
+const historyContainer = document.getElementById("callHistory-container");
+const clickCallData =[];
+document.getElementById("national-call-btn")
+.addEventListener("click",function(event){
+    let availableCoin=parseInt(totalCoin.innerText);
+    if(availableCoin >= 20){
+        totalCoin.innerText=availableCoin-20;
+        alert(`BD Emergency Service calling 
+            ${nationalTitle.innerText}\:${nationalNumber.innerText}
+            `)
+        const data={
+            name:"National Emergency No",
+            number:document.getElementById("national-number").innerText,
+            dateAndTime:new Date().toLocaleTimeString()
+        }
+        clickCallData.push(data)
+        const historyItem = document.createElement("div");
+        historyItem.className = "bg-[#FAFAFA] p-2 shadow-[0px_2px_3px_0px_#00000026] rounded-md flex justify-between mt-2 md:mt-1";
+        historyItem.innerHTML = `
+            <div>
+                <h2 class="text[#f2f2f2] font-semibold text-center">${data.name}</h2>
+                <p>${data.number}</p>
+            </div>
+            <p class="flex text-center items-center">${data.dateAndTime}</p>
+        `;
+        historyContainer.appendChild(historyItem);
+    }
+    else{
+        alert("you have not enough balance")
+    }
+});
+
+document.getElementById("clear-history")
+.addEventListener("click", function(event){
+    historyContainer.innerHTML=" ";
+})
