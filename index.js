263b9d3e655fb98ef4269ede09c7a21dde901180
saikatse85 <saikatse@gmail.com>
2025-08-27 08:03:15 +0600

    Added index.js file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("index.js file added");
+const loveAvailable = document.getElementById("love-count")
+
+document.getElementById("love-btn")
+.addEventListener("click",function(event){
+    console.log("love button clicked");  
+})
