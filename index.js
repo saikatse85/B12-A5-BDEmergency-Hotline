15d551e0f6a2e9d1d93469ee91df11c49e410849
saikatse85 <saikatse@gmail.com>
2025-08-29 14:23:44 +0600

    added copy button all id

--- index.js
+++ index.js
@@ -73,31 +73,33 @@ document.getElementById("clear-history")
 })
 
 // copy button clicked common function
-const copyCountNumber =document.getElementById("no-copy-count")
-let copyCount =0;
-function copyButtonClicked (buttonId, titleId, numberId, serviceName){
-    document.getElementById(buttonId)
-    .addEventListener("click", function(buttonId){
-        copyCount++;
-        copyCountNumber.innerText=copyCount;
+const copyCountNumber = document.getElementById("no-copy-count");
+let copyCount = 0;
 
-        const serviceTitle =document.getElementById(titleId).innerText;
-        const serviceNumber =document.getElementById(numberId).innerText;
+function copyButtonClicked(buttonId, titleId, numberId, serviceName) {
+    document.getElementById(buttonId)
+    .addEventListener("click", function() { 
+        copyCount++;
+        copyCountNumber.innerText = copyCount;
+
+        const serviceTitle = document.getElementById(titleId).innerText;
+        const serviceNumber = document.getElementById(numberId).innerText;
         
-navigator.clipboard.writeText(serviceNumber)
-                .then(() => {
-                    alert(`Number Successfully Copied
+        navigator.clipboard.writeText(serviceNumber)
+            .then(() => {
+                alert(`Number Successfully Copied
 ${serviceTitle}: ${serviceNumber}`);
-                })
-                .catch(err => console.error("Copy failed:", err));
-        });
+            })
+            .catch(err => console.error("Copy failed:", err));
+    });
 }
+
 copyButtonClicked("national-copy-btn", "national-title", "national-number", "National Emergency No");
 copyButtonClicked("police-copy-btn", "police-title", "police-number", "Police Helpline No");
 copyButtonClicked("fire-copy-btn", "fire-title", "fire-number", "Fire Service No");
 copyButtonClicked("ambulance-copy-btn", "ambulance-title", "ambulance-number", "Ambulance Service No");
 copyButtonClicked("women-child-copy-btn","women-child-title","women-child-number","Women & Child Helpline");
-copyButtonClicked("anti-corruption-copy-btn","anti-title","anti-number","Anti-Corruption Helpline");
+copyButtonClicked("anti-copy-btn","anti-title","anti-number","Anti-Corruption Helpline");
 copyButtonClicked("electricity-copy-btn","electricity-title","electricity-number","Electricity Helpline");
 copyButtonClicked("brac-copy-btn","brac-title","brac-number","Brac Helpline");
 copyButtonClicked("railway-copy-btn","railway-title","railway-number","Bangladesh Railway Helpline");
